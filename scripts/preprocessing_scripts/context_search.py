8964317dc3b24baa260a0b7c1bb535ebba4397f1
Pragalbha Lakshmanan <36824197+pragal18@users.noreply.github.com>
2020-03-08 00:00:44 +0100

    Create context_search.py

---
+++ scripts/preprocessing_scripts/context_search.py
@@ -0,0 +1,27 @@
+#Separating contents of context file downloaded from NIF DBpedia 
+import os
+import sys
+previousSubject=None
+path=sys.argv[1]
+search=sys.argv[2]
+search=search.split(".")[0]
+print(search.title())
+check=path[len(path)-6:len(path)-4]
+if check=="en":
+	i=29
+else:	
+	i=32
+with open(path,encoding="utf-8") as openfileobject:
+	for line in openfileobject:
+		if 'dbpedia.org/resource/'+search.title()+'' in line:
+			s=line.find('?')
+			thisSubject=line[i:s]
+			if previousSubject==thisSubject:
+				f.write(line);
+			else:
+				try:  
+					f=open("Files/Input"+check+"/"+thisSubject+".ttl",'a',encoding="utf-8")
+					f.write(line);
+				except:
+					pass  
+				
