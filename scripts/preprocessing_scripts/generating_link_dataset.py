4be476ee5b5b822a2fd94291e0651cd59750145a
Pragalbha Lakshmanan <36824197+pragal18@users.noreply.github.com>
2020-05-10 22:38:16 +0200

    Add files via upload

--- scripts/preprocessing_scripts/generating_link_dataset.py
+++ scripts/preprocessing_scripts/generating_link_dataset.py
@@ -7,6 +7,10 @@ from rdflib import Graph
 from nltk.tag.stanford import StanfordPOSTagger
 path=sys.argv[1]
 lang=path[len(path)-6:len(path)-4]
+if lang=="en":
+	link="https://dbpedia.org/resource/"
+else:
+	link="https://"+lang+".dbpedia.org/resource/"
 fields=	['Surface-Form', 'Link', 'Part-of-Speech'] 
 fp=open('Files/LinkDataset'+lang+'_with_duplicates.csv','w',encoding="utf-8",newline='')
 with open(path,encoding="utf-8") as openfileobject:
@@ -17,14 +21,14 @@ with open(path,encoding="utf-8") as openfileobject:
 				f=line.index('"')+1
 				s=line.index('"', line.index('"') + 1)
 				sf=line[f:s]
-				word=sf.replace(" ","_")
+				word=sf.replace(" ","_").capitalize()
 				tokens=nltk.word_tokenize(sf)
 				tag=nltk.pos_tag(tokens)
 				lists=[]
 				for i in tag:
 					lists.append(i[1])
 					combined =' '.join(lists)
-				data = [[str(sf), "https://"+lang+".wikipedia.org/wiki/"+word,combined]]
+				data = [[str(sf),link+word,combined]]
 				a.writerows(data)
 	
 
